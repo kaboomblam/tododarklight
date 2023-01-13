ebd7f9e244b0d76a723321eba8697ada9e512e59
josh <43600569+kaboomblam@users.noreply.github.com>
2023-01-13 22:52:50 +0200

    Remove sass and temporary css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-// import "./css/theme.css";
-import "./css/style.css";
+// import "./css/m3/theme.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
+      <button>Toggle Scheme</button>
       <h1 className="headline-large">Hello "TODO-dark-light" App</h1>
       <h1>Hello "TODO-dark-light" App (without Material Design Class)</h1>
     </div>
