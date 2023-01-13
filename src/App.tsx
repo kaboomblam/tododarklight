250b0f0f76281365a92d8f35b49f0a2081555156
josh <43600569+kaboomblam@users.noreply.github.com>
2023-01-14 00:22:57 +0200

    Add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,18 @@ function App() {
 
   // document.body.classList.add() = isDark ? ".dark" : "";
   return (
-    <div>
-      <button onClick={(c) => setIsDark(!isDark)}>
+    <div className="p-1">
+      <button
+        onClick={(c) => setIsDark(!isDark)}
+        className="bg-blue-400 p-1 rounded-sm"
+      >
         Toggle Scheme {isDark ? "Light" : "Dark"}
       </button>
       <h1 className="headline-large">Hello "TODO-dark-light" App</h1>
       <h1>Hello "TODO-dark-light" App (without Material Design Class)</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello Tailwind has been added.
+      </h1>
     </div>
   );
 }
