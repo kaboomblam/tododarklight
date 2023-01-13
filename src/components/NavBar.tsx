c8ce7060193b0c54e76f719a34a33f84e38102c4
josh <43600569+kaboomblam@users.noreply.github.com>
2023-01-14 01:31:25 +0200

    First step to make a navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const NavBar = () => {
-  return <nav>NavBar goes here...</nav>;
+  return (
+    <nav className="flex items-center justify-center gap-2 p-1 on-surface surface-text">
+      <p className="headline-small">Fuschia</p>
+      <ul className="flex gap-1 title-medium bg-red-100 justify-end">
+        <li>TODOS</li>
+        <li>Deleted</li>
+        <li>Completed</li>
+        <li>About</li>
+      </ul>
+      <button>Toggle Light Mode</button>
+    </nav>
+  );
 };
 
 export default NavBar;
