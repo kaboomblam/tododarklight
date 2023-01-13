f38a51561a202740bf2e749ebd23cbb4ee30519f
josh <43600569+kaboomblam@users.noreply.github.com>
2023-01-14 01:52:21 +0200

    Work with material design system for toggle button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,13 +4,13 @@ const NavBar = () => {
   return (
     <nav className="flex items-center justify-center gap-2 p-1 on-surface surface-text">
       <p className="headline-small">Fuschia</p>
-      <ul className="flex gap-1 title-medium bg-red-100 justify-end">
+      <ul className="flex gap-1 title-medium">
         <li>TODOS</li>
         <li>Deleted</li>
         <li>Completed</li>
         <li>About</li>
       </ul>
-      <button>Toggle Light Mode</button>
+      <button className="btn-primary">Toggle Light Mode</button>
     </nav>
   );
 };
