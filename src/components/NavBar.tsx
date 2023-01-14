2c3e1d3c5f09d29a764937139cf160d8b02163aa
josh <43600569+kaboomblam@users.noreply.github.com>
2023-01-14 14:58:02 +0200

    Proceed to style navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,23 +6,25 @@ const NavBar = () => {
   const theme = useContext(ThemeContext);
 
   return (
-    <nav className="flex items-center justify-center gap-2 p-1 on-surface surface-text">
-      <p className="headline-small">Fuschia todos</p>
-      <ul className="flex gap-1 title-medium">
-        <li>TODOS</li>
-        <li>Deleted</li>
-        <li>Completed</li>
-        <li>About</li>
-      </ul>
-      <button
-        className="btn-primary"
-        onClick={() => theme.setIsDark(!theme.isDark)}
-      >
-        <p className="flex items-center">
-          Toggle {theme.isDark ? "Light" : "Dark"} Mode{" "}
-          {theme.isDark ? <MdLightMode /> : <MdDarkMode />}
-        </p>
-      </button>
+    <nav className="p-1 on-surface surface-text">
+      <div className="mx-auto md:container flex items-center justify-between gap-2 bg-red-500/0">
+        <p className="headline-small">Fuschia todos</p>
+        <ul className="flex gap-2.5 title-medium bg-green-500/0">
+          <li>TODOS</li>
+          <li>Deleted</li>
+          <li>Completed</li>
+          <li>About App</li>
+        </ul>
+        <button
+          className="btn-primary"
+          onClick={() => theme.setIsDark(!theme.isDark)}
+        >
+          <p className="flex items-center">
+            Toggle {theme.isDark ? "Light" : "Dark"} Mode{" "}
+            {theme.isDark ? <MdLightMode /> : <MdDarkMode />}
+          </p>
+        </button>
+      </div>
     </nav>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,10 +3,10 @@ import NavBar from "../components/NavBar";
 
 const HomePage = () => {
   return (
-    <div>
+    <>
       <NavBar />
       <h1>HomePage items go over here...</h1>
-    </div>
+    </>
   );
 };
 
