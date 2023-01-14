2e84d03d3c4602bfb5f1e6af9dc66c05c5a6a41b
josh <43600569+kaboomblam@users.noreply.github.com>
2023-01-15 01:51:38 +0200

    Tweak note style for list element

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -12,14 +12,30 @@ const TodoListItem: React.FC<Props> = ({ todoItem }) => {
   return (
     <div
       key={todo.id}
-      className="flex flex-col secondary-container on-secondary-container-text p-1 rounded-md"
+      className="flex flex-col gap-1 secondary-container on-secondary-container-text p-1.5 rounded-md"
     >
-      <div className="flex gap-1">
-        {todo.completed && <MdOutlineDoneOutline />}
-        {todo.pinned && <AiFillPushpin />}
+      <div className="flex gap-2.5">
+        {todo.completed && (
+          <div className="flex items-center gap-1">
+            <MdOutlineDoneOutline /> Completed
+          </div>
+        )}
+        {todo.pinned && (
+          <div className="flex items-center gap-1">
+            <AiFillPushpin /> Pinned
+          </div>
+        )}
       </div>
-      <h1 className="headline-small">{todo.title.toUpperCase()}</h1>
-      <p className="body-large">{todo.note}</p>
+      <p className="body-small opacity-75">{todo.created.toDateString()}</p>
+      <div className="flex">
+        <h1 className="headline-small">{todo.title.toUpperCase()}</h1>
+      </div>
+      <p className="body-large bg-yellow-100/30 p-1 rounded">{todo.note}</p>
+      <div className="flex gap-1">
+        <button className="btn-primary">Edit</button>
+        <button className="btn-primary">Complete</button>
+        <button className="btn-primary">Pin</button>
+      </div>
     </div>
   );
 };
