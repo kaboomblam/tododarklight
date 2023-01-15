10dbce561b9cb70d6e47fc9a8079aaf2f95d19f4
josh <43600569+kaboomblam@users.noreply.github.com>
2023-01-15 12:04:12 +0200

    Major: remove material 3 styles
    Await for layout to complete and re-attempt to apply Material 3

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -12,10 +12,7 @@ type Props = {
 const TodoListItem: React.FC<Props> = ({ todoItem }) => {
   var todo = todoItem.todo;
   return (
-    <div
-      key={todo.id}
-      className="flex flex-col gap-1 tertiary-container on-secondary-container-text p-1.5 rounded-md"
-    >
+    <div key={todo.id} className="flex flex-col gap-1 p-1.5 rounded-md">
       <div className="flex gap-2.5 items-start">
         {todo.pinned && (
           <div className="flex items-center gap-1">
@@ -36,19 +33,17 @@ const TodoListItem: React.FC<Props> = ({ todoItem }) => {
       <p className="body-small opacity-75">{todo.created.toDateString()}</p>
 
       <div className="flex">
-        <h1 className="headline-small on-tertiary-container-text">
-          {todo.title.toUpperCase()}
-        </h1>
+        <h1 className="">{todo.title.toUpperCase()}</h1>
       </div>
       {todo.note && <p className="mb-1 opacity-80">{todo.note}</p>}
       <div className="flex gap-1 items-stretch justify-evenly">
-        <button className="on-surface-variant outline-variant-text rounded-sm px-2 flex-1 flex items-center gap-1">
+        <button className="rounded-sm px-2 flex-1 flex items-center gap-1">
           <MdModeEditOutline size={18} />
-          <p className="pt-0.5 flex-1 body-medium">Edit</p>
+          <p className="pt-0.5 flex-1">Edit</p>
         </button>
-        <button className="flex-[2] flex items-center gap-1 outline-variant on-surface-variant-text border-none rounded-sm p-2">
+        <button className="flex-[2] flex items-center gap-1 border-none rounded-sm p-2">
           <MdOutlineDoneOutline size={18} />{" "}
-          <p className="pt-0.5 flex-1 body-medium">Complete</p>
+          <p className="pt-0.5 flex-1">Complete</p>
         </button>
       </div>
     </div>
