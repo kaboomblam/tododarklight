8f91ca8491266e4e4b2f9e511b71ed2c4858cc26
josh <43600569+kaboomblam@users.noreply.github.com>
2023-01-17 13:09:12 +0200

    Tweak filter bar styles and theme responsiveness

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Todo } from "../utils/todo";
 import TodoListCardItem from "./TodoListCardItem";
 import { AiFillCalendar, AiFillFlag } from "react-icons/ai";
-import { FaListAlt } from "react-icons/fa";
+import { FaCalendarAlt, FaFlag, FaListAlt } from "react-icons/fa";
+import { ImCalendar, ImFlag, ImList } from "react-icons/im";
 
 type Props = {
   todos: Todo[];
@@ -12,10 +13,10 @@ const TodoList: React.FC<Props> = ({ todos }) => {
   return (
     <div className="mx-auto max-w-lg flex flex-col gap-2">
       <div className="flex gap-1 bg-blue-300/50 rounded mb-1 p-2">
-        <p className="flex justify-end items-center bg-blue-500/0">
-          <AiFillFlag className="bg-red-500/0" />
+        <p className="flex justify-end items-center bg-blue-500/0 text-indigo-600/70 dark:text-indigo-300/80 opacity-70">
+          <ImFlag className="bg-red-500/0" />
         </p>
-        <select className="font-bold bg-stone-300 text-stone-700 rounded ring-transparent outline-none">
+        <select className="py-1 px-1.5 font-bold bg-indigo-300 dark:bg-indigo-300/50 text-violet-800/80 dark:text-inherit rounded-full outline-none">
           <option value="All" selected>
             All
           </option>
@@ -24,10 +25,10 @@ const TodoList: React.FC<Props> = ({ todos }) => {
           <option value="P2">P2</option>
           <option value="P3">P3</option>
         </select>
-        <p className="ml-0.5 flex justify-end items-center bg-blue-500/0">
-          <AiFillCalendar className="bg-red-500/0" />
+        <p className="ml-1.5 flex justify-end items-center bg-blue-500/0 text-indigo-600/70 dark:text-indigo-300/80 opacity-70">
+          <ImCalendar className="bg-red-500/0" />
         </p>
-        <select className="font-bold bg-stone-300 text-stone-700 rounded outline-none">
+        <select className="py-1 px-1.5 font-bold bg-indigo-300 dark:bg-indigo-300/50 text-violet-800/80 dark:text-inherit rounded-full outline-none">
           <option value="All" selected>
             All
           </option>
@@ -40,10 +41,10 @@ const TodoList: React.FC<Props> = ({ todos }) => {
           <option value="year-1">1 year</option>
           <option value="year-1">Overdue</option>
         </select>
-        <p className="flex flex-1 justify-end items-center bg-blue-500/0">
-          <FaListAlt className="bg-red-500/0" />
+        <p className="flex-1 flex justify-end items-center bg-blue-500/0 text-indigo-600/70 dark:text-indigo-300/80 opacity-70">
+          <ImList className="bg-red-500/0" />
         </p>
-        <select className="font-bold bg-stone-300 text-stone-700 rounded outline-none">
+        <select className="py-1 px-1.5 font-bold bg-indigo-300 dark:bg-indigo-300/50 text-violet-800/80 dark:text-inherit rounded-full outline-none">
           <option value="All" selected>
             All
           </option>
