5b71c698becadc9a7594d2958c0df7a22da8dca9
josh <43600569+kaboomblam@users.noreply.github.com>
2023-01-18 13:32:58 +0200

    Refactor filter bar to separate file

---
+++ src/components/FilterBar.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { ImCalendar, ImFlag, ImList } from "react-icons/im";
+
+type Props = {};
+
+type FilterOption = {
+  icon: JSX.Element;
+  name: string;
+  values: string[];
+};
+
+const filterOptions: FilterOption[] = [
+  {
+    icon: <ImFlag className="bg-red-500/0" />,
+    name: "priority",
+    values: ["All", "P0", "P1", "P2", "P3"],
+  },
+  {
+    icon: <ImCalendar className="bg-red-500/0" />,
+    name: "dueDate",
+    values: [
+      "All",
+      "Today",
+      "1 week",
+      "2 weeks",
+      "2 weeks",
+      "1 month",
+      "3 month",
+      "6 month",
+      "1 year",
+      "Overdue",
+    ],
+  },
+  {
+    icon: <ImList className="bg-red-500/0" />,
+    name: "sort",
+    values: [
+      "All",
+      "A-Z",
+      "Z-A",
+      "Latest",
+      "Oldest",
+      "High Priority",
+      "Low Priority",
+    ],
+  },
+];
+
+const FilterBar = (props: Props) => {
+  return (
+    <div className="flex gap-3 bg-blue-300/50 rounded mb-1 p-2">
+      {filterOptions.map((filterOption) => {
+        return (
+          <div className="flex gap-0.5 last:bg-red-400/0 last:flex-1 last:justify-end">
+            <p className="flex justify-end items-center bg-blue-500/0 text-indigo-600/70 dark:text-indigo-300/80 opacity-70">
+              {filterOption.icon}
+            </p>
+            <select className="py-1 px-1.5 font-bold bg-indigo-300 dark:bg-indigo-300/50 text-violet-800/80 dark:text-inherit rounded-full outline-none">
+              {filterOption.values.map((option) => {
+                return (
+                  <option value={option} className="bg-stone-200 text-black">
+                    {option}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FilterBar;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Todo } from "../utils/todo";
 import TodoListCardItem from "./TodoListCardItem";
-import { AiFillCalendar, AiFillFlag } from "react-icons/ai";
-import { FaCalendarAlt, FaFlag, FaListAlt } from "react-icons/fa";
-import { ImCalendar, ImFlag, ImList } from "react-icons/im";
+import FilterBar from "./FilterBar";
 
 type Props = {
   todos: Todo[];
@@ -12,50 +10,7 @@ type Props = {
 const TodoList: React.FC<Props> = ({ todos }) => {
   return (
     <div className="mx-auto max-w-lg flex flex-col gap-2">
-      <div className="flex gap-1 bg-blue-300/50 rounded mb-1 p-2">
-        <p className="flex justify-end items-center bg-blue-500/0 text-indigo-600/70 dark:text-indigo-300/80 opacity-70">
-          <ImFlag className="bg-red-500/0" />
-        </p>
-        <select className="py-1 px-1.5 font-bold bg-indigo-300 dark:bg-indigo-300/50 text-violet-800/80 dark:text-inherit rounded-full outline-none">
-          <option value="All" selected>
-            All
-          </option>
-          <option value="P0">P0</option>
-          <option value="P1">P1</option>
-          <option value="P2">P2</option>
-          <option value="P3">P3</option>
-        </select>
-        <p className="ml-1.5 flex justify-end items-center bg-blue-500/0 text-indigo-600/70 dark:text-indigo-300/80 opacity-70">
-          <ImCalendar className="bg-red-500/0" />
-        </p>
-        <select className="py-1 px-1.5 font-bold bg-indigo-300 dark:bg-indigo-300/50 text-violet-800/80 dark:text-inherit rounded-full outline-none">
-          <option value="All" selected>
-            All
-          </option>
-          <option value="Today">Today</option>
-          <option value="week-1">1 week</option>
-          <option value="week-2">2 weeks</option>
-          <option value="month-1">1 month</option>
-          <option value="month-3">3 months</option>
-          <option value="month-6">6 months</option>
-          <option value="year-1">1 year</option>
-          <option value="year-1">Overdue</option>
-        </select>
-        <p className="flex-1 flex justify-end items-center bg-blue-500/0 text-indigo-600/70 dark:text-indigo-300/80 opacity-70">
-          <ImList className="bg-red-500/0" />
-        </p>
-        <select className="py-1 px-1.5 font-bold bg-indigo-300 dark:bg-indigo-300/50 text-violet-800/80 dark:text-inherit rounded-full outline-none">
-          <option value="All" selected>
-            All
-          </option>
-          <option value="Today">A-Z</option>
-          <option value="week-1">Z-A</option>
-          <option value="week-2">Latest</option>
-          <option value="month-1">Oldest</option>
-          <option value="month-3">High Priority</option>
-          <option value="month-6">Low Priority</option>
-        </select>
-      </div>
+      <FilterBar />
       {todos.map((listItem) => {
         return <TodoListCardItem todoItem={listItem} />;
       })}
